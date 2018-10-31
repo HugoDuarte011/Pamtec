2c758a665d189605a988b242920dfbba95b0dc25
HDuarte94 <Hugo.S.Duarte@iCloud.com>
2018-10-30 23:18:03 -0200

    Ajustes postar_certificados

--- _pamtecsite_/templats/Postar_Certificados.php
+++ _pamtecsite_/templats/Postar_Certificados.php
@@ -1,24 +1,23 @@
-<p><input class="PostFile" name="Certificado" id="updArquivo" type="file" style="width: 100%"></p>
-<p>
-   <select class="DropDown" name="Destinatario" style="width: 75%; height: 20%; align: center;">
-		<?php
-			if(!isset($user) || $user == ''){
-				  echo "
-						
-						<option value='1' selected='selected'>
-							Todos
-						</option>
-				  ";
-			} else {
-				  foreach($user as $campo => $value){
-						echo "
-							<option value={$value['ID']} selected='selected'>
-								{$value['nome']}
-							</option>";
-				  }
-			}
-		?>
-   </select>
-  </br>
-   <input class="cmdButton" value='Postar Certificado' name='cmdPostar' type='submit' style="width: 75%; height: 20%; align: center; display: block;">
+<p><input class="PostFile" name="Certificado" id="updArquivo" type="file" style="width: 75%; margin-left: auto; margin-right: auto; display: block;"></p>
+<p style="width:75%;">
+	<div class="custom-select" style="width: 100%; ">
+		<select style="width: 75%; display: block; margin-left: auto; margin-right: auto; font-size: 12pt;">
+			<option value="0" style="font-size: 20pt;">Todos</option>
+				<?php
+					if(!isset($user) || $user == ''){
+						  echo "
+							
+						  ";
+					} else {
+						  foreach($user as $campo => $value){
+								echo "
+									<option value={$value['ID']} style="font-size: 20pt;">{$value['nome']}</option>
+								";
+						  }
+					}
+				?>
+		</select>
+	</div>
+	</br>
+	<input class="cmdButton" value='Postar Certificado' name='cmdPostar' type='submit' style="width: 75%; margin-left: auto; margin-right: auto; display: block;">
 </p>
